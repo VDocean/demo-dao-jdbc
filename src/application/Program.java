223ecca45af2863641e3e141c0ad6663d2cd0cc2
vitoria nascimento <vivanascimento2020@gmail.com>
2024-05-20 10:19:57 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,13 +20,14 @@ public class Program {
 		
 		Locale.setDefault(Locale.US);
 		
-		Department obj=new Department(1,"Books");
-		System.out.println(obj);
 		
-		Seller seller=new Seller(21,"Bob","bob@gmail.com", new Date(),3000.00,obj);
+		SellerDao sellerDao=DaoFactory.createSellerDao();
+		
+		Seller seller=sellerDao.findById(3);
 	    System.out.println(seller);
 	    
-	    SellerDao sellerDao= DaoFactory.createSellerDao();
+
+
 	}
 
 }
