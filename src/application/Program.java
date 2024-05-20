24bcb177b0a03230504fc0a9c2e22ae442bce836
vitoria nascimento <vivanascimento2020@gmail.com>
2024-05-20 11:50:17 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ package application;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import model.dao.DaoFactory;
@@ -27,8 +28,14 @@ public class Program {
 		Seller seller=sellerDao.findById(3);
 	    System.out.println(seller);
 	    
-
-
+	    System.out.println("\n=== TEST 2: seller findByDepartment =====");
+		
+	    Department department=new Department(2,null);
+	    List<Seller> list=sellerDao.findByDepartment(department);
+	    
+	    for(Seller obj:list) {
+	    System.out.println(obj);
+	    }
 	}
 
 }
